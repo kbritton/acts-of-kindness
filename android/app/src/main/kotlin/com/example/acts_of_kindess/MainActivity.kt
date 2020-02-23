5bead0a393612a53c0533839841de39f1f18d0bf
Ken Britton <kenbritton@kens-air-2.lan>
2020-02-23 11:57:18 -0800

    added default Flutter project

---
+++ android/app/src/main/kotlin/com/example/acts_of_kindess/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.acts_of_kindess
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
